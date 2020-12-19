d3e787fd569df78deac7d2af24973a2c5336e27e
builovn <builovn@yandex.ru>
2020-12-19 10:54:08 +0300

    Support bot parsing costumers added

--- support_bot_app/lib/bot/support_bot.py
+++ support_bot_app/lib/bot/support_bot.py
@@ -1,5 +1,5 @@
 import os
-from support_bot_app.models import Message as Message_model
+from support_bot_app.models import Message as MessageModel, Customer as CustomerModel
 from django.utils import timezone
 from datetime import datetime as dt
 from .parser import *
@@ -30,20 +30,33 @@ class SupportBot:
             text = message.text
             message_date = dt.fromtimestamp(message.date)
             user_id = chat.id
-            user_username = chat.username
-            user_first_name = chat.first_name
-            user_last_name = chat.last_name
             message_checked = checked
             message_is_reply = is_reply
         except KeyError as e:
             raise e
 
-        new_message = Message_model(text=text,
-                                    telegram_id=user_id,
-                                    username=user_username,
-                                    first_name=user_first_name,
-                                    last_name=user_last_name,
-                                    date=message_date,
-                                    checked=message_checked,
-                                    is_reply=message_is_reply, )
+        new_message = MessageModel(text=text,
+                                   telegram_id=user_id,
+                                   date=message_date,
+                                   checked=message_checked,
+                                   is_reply=message_is_reply, )
         return new_message
+
+    @staticmethod
+    def parse_update_to_customer_model(update: dict, checked=False, is_reply=False):
+        try:
+            update = parse_update(update)
+            message = update.message
+            chat = message.chat
+            user_id = chat.id
+            first_name = chat.first_name
+            last_name = chat.last_name
+            username = chat.username
+        except KeyError as e:
+            raise e
+
+        new_customer = CustomerModel(telegram_id=user_id,
+                                     username=username,
+                                     first_name=first_name,
+                                     last_name=last_name,)
+        return new_customer
