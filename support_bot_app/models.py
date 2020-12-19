8b5311c67bf57b2f965297630acce099fefa90b2
builovn <builovn@yandex.ru>
2020-12-19 10:48:30 +0300

    Divided model Message into two models. Validators added.

--- support_bot_app/models.py
+++ support_bot_app/models.py
@@ -1,26 +1,38 @@
 from django.db import models
+from support_bot_app.lib.validators import customer_validators as cv, message_validators as mv
 
 
 class Message(models.Model):
-    text = models.TextField()
-    telegram_id = models.CharField(max_length=10)
-    username = models.CharField(max_length=32)
-    first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=64)
-    date = models.DateTimeField()
-    is_reply = models.BooleanField()
-    checked = models.BooleanField()
+    text = models.TextField(mv.validate_text)
+    telegram_id = models.CharField(max_length=10, validators=[cv.validate_telegram_id])
+    date = models.DateTimeField(validators=[mv.validate_date])
+    is_reply = models.BooleanField(validators=[mv.validate_boolean])
+    checked = models.BooleanField(validators=[mv.validate_boolean]  )
 
     def __eq__(self, other):
         if not isinstance(other, Message):
             return NotImplemented
         if self._get_pk_val() is None and other._get_pk_val() is None:
-            return self.text == self.text and\
+            return self.text == other.text and\
                    self.telegram_id == other.telegram_id and\
-                   self.username == other.username and \
-                   self.first_name == other.first_name and \
-                   self.last_name == other.last_name and \
                    self.date == other.date and \
                    self.is_reply == other.is_reply and \
                    self.checked == other.checked
+        return self._get_pk_val() == other._get_pk_val()
+
+
+class Customer(models.Model):
+    telegram_id = models.CharField(max_length=10, validators=[cv.validate_telegram_id])
+    username = models.CharField(max_length=32, validators=[cv.validate_username])
+    first_name = models.CharField(max_length=64, validators=[cv.validate_name])
+    last_name = models.CharField(max_length=64, validators=[cv.validate_name])
+
+    def __eq__(self, other):
+        if not isinstance(other, Customer):
+            return NotImplemented
+        if self._get_pk_val() is None and other._get_pk_val() is None:
+            return self.username == self.username and\
+                   self.telegram_id == other.telegram_id and\
+                   self.first_name == other.first_name and \
+                   self.last_name == other.last_name
         return self._get_pk_val() == other._get_pk_val()
